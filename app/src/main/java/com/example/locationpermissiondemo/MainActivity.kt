8057636ea77269e976419d74c9e19f446e791bb1
simon wilson <simonhanwilson@hotmail.co.uk>
2021-03-22 10:07:10 +0000

    Initial commit single permission example

---
+++ app/src/main/java/com/example/locationpermissiondemo/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.locationpermissiondemo
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            Toast.makeText(this, "You have access already", Toast.LENGTH_LONG).show()
+
+        } else {
+
+            // request permission
+
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), LOCATION_PERMISSION_CODE)
+
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == LOCATION_PERMISSION_CODE) {
+
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                Toast.makeText(this, "Permission granted location", Toast.LENGTH_LONG).show()
+
+            } else {
+
+                Toast.makeText(this, "permission has been denied", Toast.LENGTH_LONG).show()
+
+            }
+
+        }
+
+    }
+
+    companion object {
+
+        private const val LOCATION_PERMISSION_CODE = 1
+
+    }
+
+
+}
